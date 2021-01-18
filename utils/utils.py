5d767cd5c175c3d14d595392a9287260c2a4c48d
Jannes <jurie.germishuys@combine.se>
2021-01-18 12:29:35 +0100

    Comment out result plot for singularity test

--- utils/utils.py
+++ utils/utils.py
@@ -1048,4 +1048,4 @@ def plot_results(start=0, stop=0, bucket='', id=()):  # from utils.utils import 
 
     fig.tight_layout()
     ax[1].legend()
-    fig.savefig('results.png', dpi=200)
+    #fig.savefig('results.png', dpi=200)
