2ebe210c16656ad156eac89d447356e0fbbf6a56
Jannes <jurie.germishuys@combine.se>
2021-01-18 14:42:02 +0100

    Remove row from get_id

--- src/db_utils.py
+++ src/db_utils.py
@@ -89,7 +89,7 @@ def test_table(df, table_name, keys=["id"]):
             f"The table {table_name} has invalid entries, please ensure that all columns are non-zero"
         )
 
-def get_id(row, field_name, table_name, conn, conditions={"a": "=b"}):
+def get_id(field_name, table_name, conn, conditions={"a": "=b"}):
 
     # Get id from a table where a condition is met
 
