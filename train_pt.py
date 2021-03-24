9570e74ea5cf2cee5c62ce0d98723c118ebe261e
Jannes Germishuys <jurie.germishuys@combine.se>
2021-03-24 16:27:47 +0100

    Add test script and fix train imports

--- train_pt.py
+++ train_pt.py
@@ -20,18 +20,18 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
-import test  # import test.py to get mAP after each epoch
-from models.experimental import attempt_load
-from models.yolo import Model
-from utils.autoanchor import check_anchors
-from utils.datasets import create_dataloader
-from utils.general import labels_to_class_weights, increment_path, labels_to_image_weights, init_seeds, \
+import test_pt  # import test.py to get mAP after each epoch
+from models_pt.experimental import attempt_load
+from models_pt.yolo import Model
+from utils_pt.autoanchor import check_anchors
+from utils_pt.datasets import create_dataloader
+from utils_pt.general import labels_to_class_weights, increment_path, labels_to_image_weights, init_seeds, \
     fitness, strip_optimizer, get_latest_run, check_dataset, check_file, check_git_status, check_img_size, \
     check_requirements, print_mutation, set_logging, one_cycle, colorstr
-from utils.google_utils import attempt_download
-from utils.loss import compute_loss
-from utils.plots import plot_images, plot_labels, plot_results, plot_evolution
-from utils.torch_utils import ModelEMA, select_device, intersect_dicts, torch_distributed_zero_first
+from utils_pt.google_utils import attempt_download
+from utils_pt.loss import compute_loss
+from utils_pt.plots import plot_images, plot_labels, plot_results, plot_evolution
+from utils_pt.torch_utils import ModelEMA, select_device, intersect_dicts, torch_distributed_zero_first
 
 logger = logging.getLogger(__name__)
 
